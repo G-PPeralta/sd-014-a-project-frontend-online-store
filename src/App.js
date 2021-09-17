e2c257348b8cac8e4beab61ded4e127e0f9e14a4
Anna Hamann <annasoldih@gmail.com>
2021-09-17 16:52:48 -0300

    fix lint 1-2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { getCategories } from './services/api';
 import Home from './pages/home';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      results: [],
-    };
-    this.getApi = this.getApi.bind(this);
-  }
-
-  componentDidMount() {
-    this.getApi();
-  }
-
-  async getApi() {
-    const results = await getCategories();
-    this.setState = {
-      results,
-    };
-  }
-
-  render() {
-    const { results } = this.state;
-    return (
-      <div className="App">
-        <Home data-testid="home-initial-message" />
-        <header className="App-header">
-
-          <span>{ results }</span>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
-export default App;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
     return (
       <>
-        <input type="text" name="searchTextHome" id="searchTextHome"/>
+        <input type="text" name="searchTextHome" id="searchTextHome" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
       </>
-
-    )
+    );
   }
 }
