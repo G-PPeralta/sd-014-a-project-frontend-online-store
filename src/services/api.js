a393d47805912398fb95fca818b5b193502a2e8e
Marcello Pires Alves <marcello.alves@gmail.com>
2021-09-17 16:02:54 -0300

    Requisito 1 - feito pelos 4 membros em conjunto.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,24 @@
-export async function getCategories() {
-  // Implemente aqui
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB/';
+
+async function callAPI(query) {
+  try {
+    const responseRaw = await fetch(query);
+    const responseJSON = await responseRaw.json();
+    return responseJSON;
+  } catch (error) {
+    console.log(`[Erro]: ${error}`);
+    return null;
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getCategories() {
+  const CATEGORIES_ENDPOINT = `${BASE_URL}categories`;
+
+  return callAPI(CATEGORIES_ENDPOINT);
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const CATEGORY_QUERY_ITEMS = `${BASE_URL}search?category=${categoryId}&q=${query}`;
+
+  return callAPI(CATEGORY_QUERY_ITEMS);
 }
