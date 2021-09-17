3f48e7926c550523de76d34cd72b510211ec6646
Andrea <andrealh08@gmail.com>
2021-09-17 17:32:48 -0300

    Finaliza requisito 3 com todos os integrantes do grupo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/cart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import CartButton from '../components/CartButton';
 
 export default class Home extends Component {
   render() {
     return (
-      <>
-        <input type="text" name="searchTextHome" id="searchTextHome" />
+      <form>
+        <label htmlFor="searchTextHome">
+          <input type="text" name="searchTextHome" id="searchTextHome" />
+        </label>
+        <CartButton />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-      </>
+      </form>
     );
   }
 }
