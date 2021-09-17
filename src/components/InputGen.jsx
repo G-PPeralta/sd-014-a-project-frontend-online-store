ed593450d416ddeda9c0a00c1aa860ac4596c780
Pablo Assunção <pabloassuncao2@gmail.com>
2021-09-17 20:32:44 -0300

    Adiciona o requisito 2

---
+++ src/components/InputGen.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class InputGen extends Component {
+  render() {
+    const { config } = this.props;
+    const [type, name, dataTestId, value, checked, onChange, labelText,
+      className] = config;
+
+    if (checked === false) {
+      return (
+        <label htmlFor={ dataTestId } data-testid={ `${dataTestId}-label` }>
+          <span>{ labelText }</span>
+          <input
+            type={ type }
+            name={ name }
+            data-testid={ dataTestId }
+            value={ value }
+            onChange={ onChange }
+            className={ className }
+          />
+        </label>
+      );
+    }
+    return (
+      <label htmlFor={ dataTestId } data-testid={ `${dataTestId}-label` }>
+        <span>{ labelText }</span>
+        <input
+          type={ type }
+          name={ name }
+          data-testid={ dataTestId }
+          checked={ checked }
+          onChange={ onChange }
+        />
+      </label>
+    );
+  }
+}
+
+InputGen.propTypes = {
+  config: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+    PropTypes.number,
+    PropTypes.bool,
+  ])).isRequired,
+};
+
+export default InputGen;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+
+import InputGen from '../components/InputGen';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { searchText } = this.state;
     return (
       <div>
-        oi
+        <InputGen
+          config={ ['text', 'searchText', 'search-text-input', searchText, false,
+            this.handleChange, 'Faça sua pesquisa'] }
+        />
+        <div className="itemList">
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
       </div>
     );
   }
