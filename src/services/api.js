efb556ded58653ac18834d656adbd7b21ed273af
hector <hectoreddbenitez@gmail.com>
2021-09-17 17:16:00 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  render();
+  const req = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await req.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const req = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const response = await req.json();
+  return response;
 }
