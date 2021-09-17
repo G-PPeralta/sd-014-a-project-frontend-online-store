0504ccf87a6223b20073a0b06936b0b27a346f84
Fernando Nascimento Oliveira <fernandocetep@outlook.com>
2021-09-17 15:23:24 -0300

    Requisito 1 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,14 @@
 export async function getCategories() {
   // Implemente aqui
-  
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const objectReturned = await request.json();
+  return objectReturned;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const objectReturned = await request.json();
+  return objectReturned;
 }
+
