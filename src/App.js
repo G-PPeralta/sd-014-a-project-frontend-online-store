2ab06f6dc9c00e716c1acb337812bc880cebbb2b
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-09-17 18:04:46 -0300

    Redireciona para o CartList quando clicado no botao

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CartList from './components/CartList';
 import HomePage from './components/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ HomePage } />
+        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/cart" component={ CartList } />
       </Switch>
     </BrowserRouter>
   );
