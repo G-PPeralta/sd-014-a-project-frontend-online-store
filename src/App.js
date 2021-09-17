7044865c6d55a144ce3095164cfcfd3e78bd744e
Matheus Alves <malves224@gmail.com>
2021-09-17 16:25:26 -0300

    adiciona h3 teste

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
+        <h3> Olá!! </h3>
         <p>Edit src/App.js and save to reload.</p>
         <a
           className="App-link"
