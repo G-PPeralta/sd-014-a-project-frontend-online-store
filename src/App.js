3beae3e5a4063d184212bb1aeb40aa9cab848487
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-09-17 19:07:53 -0300

    Renomeia pasta page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CartList from './Pages/CartList';
-import HomePage from './Pages/HomePage';
+import CartList from './Page/CartList';
+import HomePage from './Page/HomePage';
 
 function App() {
   return (
