930a198f78d8b28900a5b175c4054145996fa645
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-17 16:30:34 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+    };
+    this.getApi = this.getApi.bind(this);
+  }
 
-        <span>{ getCategories }</span>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    this.getApi();
+  }
+
+  async getApi() {
+    const results = await getCategories();
+    this.setState = {
+      results,
+    };
+  }
+
+  render() {
+    const { results } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <span>{ results }</span>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,25 @@
+const linkBase = 'https://api.mercadolibre.com/sites/MLB/';
+
+async function getApi(search) {
+  try {
+    const response = await fetch(search);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
 export async function getCategories() {
   // Requisito feito via LiveShare no VSCode por todos os membros do grupo conjuntamente
-    const myEndPoint = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`);
-    const myInfo = await myEndPoint.json();
-    console.log(myInfo);
-    return myInfo;
-  };
+  const baseCategories = `${linkBase}categories`;
+  return getApi(baseCategories);
+}
 
-  //const getMusics = async (id) => {
-//   const request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
-//   const requestJson = await request.json();
-//   return requestJson.results;
-// };
-
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Requisito feito via LiveShare no VSCode por todos os membros do grupo conjuntamente
-  // termo     https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
-  // categoria:   https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID
-  // Para buscar itens de uma categoria por termo:
-  //  https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY <<< Compara aqui
+  const queryItems = `${linkBase}search?category=${categoryId}&q=${query}`;
+  return getApi(queryItems);
 }
