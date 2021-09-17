a77bebe243dcf7374047a475276d09218ff16dd6
MuriloM <muriloommaia@gmail.com>
2021-09-17 16:11:17 -0300

    ajuste results

--- src/services/api.js
+++ src/services/api.js
@@ -9,5 +9,6 @@ export async function getProductsFromCategoryAndQuery(query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const getQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
   const getQueryJSON = await getQuery.json();
-  return getQueryJSON;
+  console.log(getQueryJSON.results);
+  return getQueryJSON.results;
 }
