9ac01984cba91fe7ed5f184d4523bc13d5d21c87
Yan Brasileiro <yan.brasileiro@gmail.com>
2021-09-17 17:42:07 -0300

    Requisito 3

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class main extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <Link to="/shoppingCart">
+          <button
+            type="button"
+            data-testid="shopping-cart-button"
+            placeholder="carrinho"
+          >
+            carrinho
+          </button>
+        </Link>
       </div>
     );
   }
--- src/pages/shoppingCart.jsx
+++ src/pages/shoppingCart.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 class shoppingCart extends React.Component {
   render() {
     return (
-      <div>
-      </div>
+      <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
     );
   }
 }
