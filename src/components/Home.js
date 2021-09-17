e30ccfd13781ec9f35b3216c9ab7d89bb9cad66d
BeatrizR <biarl.ramalhorl@gmail.com>
2021-09-17 17:41:48 -0300

    requisito 03

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
+      <div>
+        <label htmlFor="search" data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+          <input type="text" />
+        </label>
+        <div>
+          <Link to="/ShoppingCart" data-testid="shopping-cart-button">Carrinho</Link>
+        </div>
       </div>
     );
   }
