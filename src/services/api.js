602a959a6d96821df7937d58ef119c6adc781674
MuriloM <muriloommaia@gmail.com>
2021-09-17 15:45:10 -0300

    getCategories - requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
   // Implemente aqui
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categoriesJSON = await categories.json();
+  return categoriesJSON;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  console.log('teste');
 }
