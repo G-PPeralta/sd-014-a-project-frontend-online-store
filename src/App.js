f20fa3dcb9b23bc43306a28a5f937a20f30e91b6
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-09-17 17:41:50 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/cart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
+      <div>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <span role="img" aria-label="shopping-cart">&#128722;</span>
+        </Link>
       </div>
     );
   }
