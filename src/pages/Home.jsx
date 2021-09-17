552fdd22eff728b8fdfa746a84e309ab0cdd7eef
unamednada <dias.gustavo.dias@gmail.com>
2021-09-17 17:35:51 -0300

    Acrescenta mensagem correta a homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <p>Home</p>
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
     );
   }
 }
