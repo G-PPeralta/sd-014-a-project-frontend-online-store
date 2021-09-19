1de7871ab7aa48496d1682cb951e240ad7c36ab8
Vincenzo <vincenzofdg@hotmail.com>
2021-09-19 15:23:29 -0300

    Requisito 03 => sem CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ Home } />
+        <Route path="/cart" component={ Cart } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BtnCart from '../components/BtnCart';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <BtnCart />
+      </div>
+    );
+  }
+}
+
+export default Home;
