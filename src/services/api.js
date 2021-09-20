b70bb7eaf98d171d268dfb241a0625bb4dbb7118
César Meira <cesarmeirasouza@gmail.com>
2021-09-20 02:42:25 -0300

    Req 1 fixed

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,7 @@ export async function getCategories() {
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId) {
-  const products = await (await fetch(`https://api.mercadolibre.com/categories/${categoryId}`)).json();
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const products = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)).json();
   return products;
 }
-
