98fe7c5d76b10398625ed179831317544e6658e1
Matheus Alves <malves224@gmail.com>
2021-09-20 18:54:24 -0300

    add funçao home que manipula o state para renderizar os cards, cumprindo requisito 5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,32 @@ class Home extends Component {
     this.state = {
       categories: [],
       productFiltred: [],
+      searchValue: '',
+      idCategory: '',
     };
 
     this.filterByCategory = this.filterByCategory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategories();
+  }
+
+  async handleClick() {
+    const { searchValue, idCategory } = this.state;
+    const fetchProduct = await getProductsFromCategoryAndQuery(idCategory, searchValue);
+    this.setState({
+      productFiltred: fetchProduct.results,
+    });
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   async fetchCategories() {
@@ -29,20 +48,35 @@ class Home extends Component {
     const productFiltred = await getProductsFromCategoryAndQuery(categoryId, query);
     this.setState({
       productFiltred: productFiltred.results,
+      idCategory: categoryId,
     });
   }
 
   render() {
-    const { state: { categories } } = this;
+    const { state: { categories, productFiltred, searchValue } } = this;
     return (
       <div>
-        <input data-testid="query-input" type="text" />
-        <button data-testid="query-button" type="button"> Pesquisar </button>
+        <input
+          name="searchValue"
+          value={ searchValue }
+          onChange={ this.handleChange }
+          data-testid="query-input"
+          type="text"
+        />
+        <button
+          onClick={ this.handleClick }
+          data-testid="query-button"
+          type="button"
+        >
+          Pesquisar
+        </button>
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
         <section>
-          <ProductCard />
+          { productFiltred.map((product) => (
+            <ProductCard key={ product.id } product={ product } />
+          )) }
         </section>
         <ListCategories
           filterByCategory={ this.filterByCategory }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class ProductCard extends Component {
   render() {
+    const { product } = this.props;
     return (
       <div data-testid="product">
-        <p>cards</p>
+        <h1>{ product.title }</h1>
+        <img src={ product.thumbnail } alt="imagem do produto" />
+        <p>{ product.price }</p>
       </div>
     );
   }
 }
 
 ProductCard.propTypes = {
-
+  product: PropTypes.shape({
+    title: PropTypes.string,
+    thumbnail: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
 };
 
 export default ProductCard;
