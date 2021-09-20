dc55f81d26dcfd818f89920e509068d04b8944fb
Monnylly <monnylly@gmail.com>
2021-09-20 17:26:16 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
-      <span data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </span>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/shopping-cart" component={ ShoppingCart } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
