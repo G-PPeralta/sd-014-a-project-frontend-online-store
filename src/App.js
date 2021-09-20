4b7d9c414edc3b2481b31e279a12b5ad871e600a
César Meira <cesarmeirasouza@gmail.com>
2021-09-20 15:29:31 -0300

    Req 1 - 2 - 4 - cesar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
-import { NotFound } from 'http-errors';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Main } />
-          <Route exact path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Main } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,35 +8,38 @@ class Main extends React.Component {
     this.state = {
       categories: [],
       fetched: false,
-    }
-    
+    };
+
     this.fetchCategories = this.fetchCategories.bind(this);
     this.showCategories = this.showCategories.bind(this);
   }
-  
-async fetchCategories() {
-  const categories = await getCategories();
-  this.setState({
-    categories,
-    fetched: true,
-  });
-}
 
-showCategories() {
-  const { categories } = this.state;
-  return  categories.map((category) => <p data-testid="category" key={ category.id }>{ category.name }</p>); 
-}
+  componentDidMount() {
+    this.fetchCategories();
+  }
 
-componentDidMount() {
-  this.fetchCategories();
-}
+  showCategories() {
+    const { categories } = this.state;
+    return categories.map((cat) => (
+      <p data-testid="category" key={ cat.id }>{ cat.name }</p>));
+  }
+
+  async fetchCategories() {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+      fetched: true,
+    });
+  }
 
   render() {
     const { fetched } = this.state;
     return (
       <form>
         <input type="text" />
-        <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
         { fetched && this.showCategories() }
       </form>
     );
