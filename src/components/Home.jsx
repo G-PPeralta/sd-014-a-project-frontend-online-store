679ed47ffb2db3553ed5585aca9fbf3531ba2f58
hector <hectoreddbenitez@gmail.com>
2021-09-20 16:00:00 -0300

    Requisito 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,9 @@ class Home extends Component {
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          Carrinho
+        </Link>
       </div>
     );
   }
