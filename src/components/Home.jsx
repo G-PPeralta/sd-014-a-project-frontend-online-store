235b98e6d94ba4f54b895b1279db91d82db4bdc6
Matheus Alves <malves224@gmail.com>
2021-09-20 16:18:30 -0300

    adiciona atributos nescesarios e cria component para o requisito 5

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getCategories } from '../services/api';
 import ListCategories from './ListCategories';
+import ProductCard from './ProductCard';
 
 class Home extends Component {
   constructor() {
@@ -25,10 +26,14 @@ class Home extends Component {
     const { state: { categories } } = this;
     return (
       <div>
-        <input type="text" />
+        <input data-testid="query-input" type="text" />
+        <button data-testid="query-button" type > Pesquisar </button>
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <section>
+          <ProductCard />
+        </section>
         <ListCategories categories={ categories } />
       </div>
     );
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends Component {
+  render() {
+    return (
+      <div data-testid="product">
+        <p>cards</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+
+};
+
+export default ProductCard;
