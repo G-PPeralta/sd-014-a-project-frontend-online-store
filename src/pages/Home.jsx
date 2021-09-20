c2fc9ded241939c49995aaa64f5c316871c9b7fb
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-20 02:20:51 -0300

    2 - Create an empty product listing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <input id="product" name="product" type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
