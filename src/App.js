f0b1a16f59bceb0583449e14bad1d99785ebaa7a
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-09-20 14:59:01 -0300

    requisito 04 implementa lista de categorias na home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
-import ProductsList from './pages/ProductsList';
 import ShoppingCart from './pages/ShoppingCart';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route exact path="/" component={ ProductsList } />
+      <Route exact path="/" component={ Home } />
       <Route path="/shopping-cart" component={ ShoppingCart } />
     </BrowserRouter>
   );
