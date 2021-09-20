1efc9208ed0fd39ccd71b8f099ad44e7d3720450
vixemari <marianarocha308@gmail.com>
2021-09-20 18:03:11 -0300

    Requesito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   render() {
@@ -7,6 +8,9 @@ class Search extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="/Cart" data-testid="shopping-cart-button">
+          <button type="submit">Icone do Carrinho</button>
+        </Link>
       </section>
     );
   }
