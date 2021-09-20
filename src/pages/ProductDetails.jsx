cee1caec54f381627a6f79c69d978e33b422de3f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-20 14:41:40 -0300

    Verificando o requisito 7

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class ProductDetails extends React.Component {
   constructor() {
     super();
+    this.categoryAndQuery = this.categoryAndQuery.bind(this);
+  }
+
+  componentDidMount() {
+    this.categoryAndQuery();
+  }
+
+  async categoryAndQuery() {
+    const endipoint = await getProductsFromCategoryAndQuery();
+    console.log(endipoint);
+    return endipoint;
   }
 
   render() {
+    console.log(this);
     const { product } = this.props;
-    console.log(product);
     return (
       <div>
         <p>pagina de detalhes</p>
-        <h3>Nome do produto</h3>
+        <h3 data-testid="product-detail-name">Nome do produto</h3>
         <img src="" alt="" />
         <spam>imagem</spam>
         <p>preço</p>
