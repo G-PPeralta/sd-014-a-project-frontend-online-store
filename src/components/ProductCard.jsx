71a3eed6b4e49f025ef7f44474612317504a05a6
Caio César Amorim <caiocavalcante063@gmail.com>
2021-09-20 18:09:13 -0300

    cria botao de adicionar ao carrinho

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { BagPlusFill } from 'react-bootstrap-icons';
 
 export default class ProductCard extends Component {
+  handleClick(title) {
+    localStorage.setItem('meuGato', title);
+  }
+
   render() {
     const { product: { title, thumbnail, price } } = this.props;
     return (
@@ -9,6 +14,13 @@ export default class ProductCard extends Component {
         <h2>{ title }</h2>
         <img src={ thumbnail } alt={ `foto do produto ${title}` } />
         <h2>{ `preço: ${price}` }</h2>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={ this.handleClick }
+        >
+          <BagPlusFill />
+        </button>
       </div>
     );
   }
