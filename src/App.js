4e1300d330cc44f53510b7bad985d2a1424f5663
BrianCaetano <brian-caetano@hotmail.com>
2021-09-20 17:00:36 -0300

    req 7 (Brian e Henrique)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import ProductDetails from './pages/ProductDetails';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/cart" component={ Cart } />
+        <Route exact path="/product-details/:id" component={ ProductDetails } />
       </Switch>
     </BrowserRouter>
   );
