e2bf3bd74d26becb029d2d90bf77e47826736092
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-09-20 16:30:01 -0300

    Finaliza requisito 3

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+        <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+          <button type="button">Adicionar icone do carrinho aqui</button>
+        </Link>
       </div>
     );
   }
 }
+
 export default Home;
