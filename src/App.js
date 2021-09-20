df97000107d31c20f27cd3b52f809e2c05df128f
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-09-20 14:59:38 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Home, CartPage } from './pages';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route path="/cart" component={ CartPage } />
+          <Route exact path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
     </div>
