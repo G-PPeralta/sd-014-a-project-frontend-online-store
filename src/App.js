35981bf927993264e41be88ae9773ee4fba98cf1
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-20 14:12:31 -0300

    Resolve problema de css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
