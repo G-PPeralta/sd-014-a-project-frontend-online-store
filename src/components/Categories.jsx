e43a4a1d2f3da1a62bf368b9eaaa49a444885b27
Marcio <marciofonseca.ufrj@gmail.com>
2021-09-20 19:10:29 -0300

    Req 06

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Categories extends Component {
   constructor() {
@@ -7,12 +7,25 @@ class Categories extends Component {
 
     this.state = {
       categoriesList: [],
+      productsList: [],
     };
     this.fetchCategories = this.fetchCategories.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.fetchAPI = this.fetchAPI.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategories();
+  }
+
+  onClick(event) {
+    this.fetchAPI(event.target.value);
+  }
+
+  fetchAPI(category) {
+    getProductsFromCategoryAndQuery(category, '').then((query) => {
+      this.setState({ productsList: query.results });
+    });
   }
 
   fetchCategories() {
@@ -24,11 +37,30 @@ class Categories extends Component {
   }
 
   render() {
-    const { categoriesList } = this.state;
+    const { categoriesList, productsList } = this.state;
     return (
       <div>
-        {categoriesList.map(({ name, id }) => (
-          <p data-testid="category" key={ id }>{name}</p>
+        {categoriesList.map((categorie) => (
+          <div key={ categorie.id }>
+            <label htmlFor={ categorie.id }>
+              <input
+                data-testid="category"
+                type="radio"
+                id={ categorie.id }
+                onClick={ this.onClick }
+                name="categorie"
+                value={ categorie.id }
+              />
+              {categorie.name}
+            </label>
+          </div>
+        ))}
+        { productsList.map((product) => (
+          <div data-testid="product" key={ product.id }>
+            <h3>{ product.title }</h3>
+            <img src={ product.thumbnail } alt={ product.title } />
+            <p>{ product.price }</p>
+          </div>
         ))}
       </div>
     );
