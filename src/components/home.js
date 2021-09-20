0fca74b905d3dd97356206c60406932193781c98
WalmirLucena <walmirlucena3@gmail.com>
2021-09-20 12:14:38 -0400

    Adiciona Requisitos 1 e 2

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <h1>Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+      </div>
+    );
+  }
+}
+export default Home;
