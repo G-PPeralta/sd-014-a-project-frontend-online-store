2b29b8b4cb578d3019d27f7db7bce177d7db9712
Luiz Seidl <luizseidl@gmail.com>
2021-09-21 15:36:08 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
+import ProductDetails from './pages/ProductDetails';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/shopping-cart" component={ ShoppingCart } />
+        <Route path="/details/:id" component={ ProductDetails } />
       </Switch>
     </BrowserRouter>
   );
