7ae70a767056b1f86fa83da95227b26575b33669
Dayane <dayanebmartins@gmail.com>
2021-09-21 19:13:53 -0300

    desenvolvendo requisito 8

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
 
 class AddToCart extends Component {
-  constructor (){
-    super();
-    this.state ={
-       cartItems:[],
-    }
-  }
-
-  // saveLocalStorage= (item) =>{
-  //  localStorage.setItem('cart',JSON.stringify(item));
-  // }
+ 
 
   handleClick = (event) => {
-    const { cartItems } = this.state;
-    const cart= cartItems;
-    console.log(cart);
-    // this.setState(cartItems:([...cart ,event.target.value]));
+    const { getItem } = this.props;
+  getItem(event.target.value);
   }
 
   render() {
     const { itemTitle, itemPrice } = this.props;
-    const {cartItems}= this.state;
     return (
       <div>
         <button
--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -3,16 +3,32 @@ import PropTypes from 'prop-types';
 import CardProduct from './CardProduct';
 
 class SearchProduct extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItems: [],
+    }
+  }
+
+ getItem =(item)=>{
+   const {cartItems}= this.state;
+   const cart = (cartItems);
+  
+ this.setState ({cartItems:([...cart,item])});
+ localStorage.setItem("setCart",cartItems);
+ }
+
   searchProduct = (arrayProduct) => {
     if (arrayProduct.length !== 0) {
       return (
         <div>
           {arrayProduct.map((products) => (
             <CardProduct
-              key={ products.id }
-              title={ products.title }
-              image={ products.thumbnail }
-              price={ products.price }
+              key={products.id}
+              title={products.title}
+              image={products.thumbnail}
+              price={products.price}
+              getItem={this.getItem}
             />))}
         </div>
       );
