be74296e6246096b1dafff0599331bafb26907aa
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-21 15:49:30 -0300

    7 - Adjustments

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/ProductDetail.css';
 
-class ProductDetail extends Component {
+export default class ProductDetail extends Component {
   constructor(props) {
     super(props);
-    this.state = props.location.state; // https://cursos.alura.com.br/forum/topico-this-props-location-query-em-novas-versoes-48309
+    this.state = {
+      product: props.location.state,
+    // https://cursos.alura.com.br/forum/topico-this-props-location-query-em-novas-versoes-48309
+    };
   }
 
   render() {
-    const { title, thumbnail, price } = this.state;
+    const {
+      product: { title, thumbnail, price },
+    } = this.state;
 
     return (
-      <div data-testid="product-detail-name">
+      <div className="product-detail" data-testid="product-detail-name">
         <h3>{title}</h3>
         <img alt={ title } className="product-thumbnail" src={ thumbnail } />
         <p>{`R$${price.toFixed(2)}`}</p>
-      </div>);
+      </div>
+    );
   }
 }
 
 ProductDetail.propTypes = {
   location: PropTypes.object,
 }.isRequired;
-
-export default ProductDetail;
