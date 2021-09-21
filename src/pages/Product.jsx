1e7478e0945711069fd2e897814142fb4ec712f9
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-09-21 18:02:46 -0300

    requisito 7

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,13 +3,38 @@ import React, { Component } from 'react';
 class Product extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    const { location: { state: { product } } } = props;
+    this.state = {
+      product,
+    };
   }
+
   render() {
+    const { product } = this.state;
+    const { attributes } = product;
     return (
-      <p>Olá Mundo</p>
+      <div>
+        <h1
+          data-testid="product-detail-name"
+        >
+          {`${product.title} - $${product.price}`}
+        </h1>
+        <img src={ product.thumbnail } alt={ product.title } />
+        <section className="specs-product">
+          <h3>Especificações técnicas</h3>
+          <ol>
+            { attributes.map((attribute) => (
+              <li key={ attribute.id }>
+                {
+                  `${attribute.name} - ${attribute.value_name ?? 'indefinido'}`
+                }
+              </li>
+            )) }
+          </ol>
+        </section>
+      </div>
     );
   }
 }
- 
+
 export default Product;
