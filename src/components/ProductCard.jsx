c73f9edcc0e53e53e38130fe0d9feea98ebf86fd
Gustavo Dolzan <gudolzan@gmail.com>
2021-09-21 16:14:27 -0300

    Bug fix

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -72,8 +72,8 @@ export default class ProductCard extends Component {
           <h3 className="product-title">{title}</h3>
           <img alt={ title } className="product-thumbnail" src={ thumbnail } />
           <p>{`R$${price.toFixed(2)}`}</p>
-          {this.addToCartBtn()}
         </Link>
+        {this.addToCartBtn()}
       </div>
     );
   }
