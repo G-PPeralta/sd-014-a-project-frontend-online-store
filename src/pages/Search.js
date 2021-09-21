ecffc476f41c0054bdf783317da80eff20e0527d
Rogério Maia Jr. [Roger] <rogeriomaiajr13@gmail.com>
2021-09-21 05:57:06 -0300

    Refatora o req 5, CategoriesList.jsx criado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CategoriesList from '../components/CategoriesList';
 
 class Search extends React.Component {
   render() {
     return (
       <section>
+        <CategoriesList />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
