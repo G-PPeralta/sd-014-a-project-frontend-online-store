7299c19f741441c9acaa8464c660ba14c7a9cacf
Pedro Silva Buente <pedrobuente@gmail.com>
2021-09-21 15:53:11 -0300

    requisito 9 completo

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -23,6 +23,21 @@ export default class ProductDetail extends Component {
     this.callApi();
   }
 
+  async handleClick(event) {
+    event.preventDefault();
+    const cartList = {
+      prodId: event.target.className,
+      name: event.target.name,
+      prodPrice: event.target.value,
+    };
+    if (!JSON.parse(localStorage.getItem('cartList'))) {
+      localStorage.setItem('cartList', JSON.stringify([]));
+    }
+    const cart = JSON.parse(localStorage.getItem('cartList'));
+    localStorage.setItem('cartList',
+      JSON.stringify([...cart, cartList]));
+  }
+
   async callApi() {
     const { location: { apiProps: { categoriaDeProduto, searchText } } } = this.props;
     const results = await getProductsFromCategoryAndQuery(categoriaDeProduto,
@@ -38,7 +53,7 @@ export default class ProductDetail extends Component {
     const myProduct = resultApi.find((result) => result.id === id);
     // console.log(myProduct);
 
-    return (myProduct ? ( // This ternary conditional is needed, to ensure we only try to acces myProduct object properties, after resultApi is a non blank array to be iterated with the find HoF
+    return myProduct ? ( // This ternary conditional is needed, to ensure we only try to acces myProduct object properties, after resultApi is a non blank array to be iterated with the find HoF
       <main className="shopping-main">
         <Header />
         <section className="cart-banner">
@@ -46,21 +61,22 @@ export default class ProductDetail extends Component {
         </section>
 
         <section className="main-sec-detail">
-          <p
-            data-testid="product-detail-name"
-            className="product-detail-name"
-          >
+          <p data-testid="product-detail-name" className="product-detail-name">
             {myProduct.title}
           </p>
           <img className="img-detail" src={ myProduct.thumbnail } alt="" />
           <p className="product-detail-p">
-            <span className="product-detail-spec-title">Aceita MercadoPago?</span>
+            <span className="product-detail-spec-title">
+              Aceita MercadoPago?
+            </span>
             <span className="product-detail-spec-descr">
               {myProduct.accepts_mercadopago ? ' Sim' : ' Não'}
             </span>
           </p>
           <p className="product-detail-p">
-            <span className="product-detail-spec-title">Quantidade Disponível:</span>
+            <span className="product-detail-spec-title">
+              Quantidade Disponível:
+            </span>
             <span className="product-detail-spec-descr">
               {myProduct.available_quantity}
             </span>
@@ -74,20 +90,25 @@ export default class ProductDetail extends Component {
           <form>
             Avaliacoes
             <input type="email" name="email-for-evaluation" />
-            <textarea
-              data-testid="product-detail-evaluation"
-            />
+            <textarea data-testid="product-detail-evaluation" />
             <button type="button">Avaliar</button>
           </form>
-          <img // this is just for fun... remove later!
-            className="mr-bean"
-            src="https://c.tenor.com/xEu4tiLm3hUAAAAd/mr-bean-mr-beans-holiday.gif"
-            alt="mr. bean dancing"
-          />
+          <button
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            name={ myProduct.title }
+            value={ myProduct.price }
+            className={ id }
+            onClick={ this.handleClick }
+          >
+            Adicionar ao Carrinho
+          </button>
         </section>
         <Footer />
       </main>
-    ) : <span />);
+    ) : (
+      <span />
+    );
   }
 }
 
