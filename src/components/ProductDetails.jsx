c8c98713389729a7d40106c75b30323a7c631b0d
AlineCarolina <eualine2017@gmail.com>
2021-09-22 20:42:05 -0300

    requisito 9

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+/* import { Link } from 'react-router-dom'; */
 
 export default class ProductDetails extends Component {
   constructor() {
@@ -23,8 +24,27 @@ export default class ProductDetails extends Component {
       });
     }
 
+    /* addProductToCart(product) {
+      let cart = JSON.parse(localStorage.getItem('cart'));
+      let check = '';
+      if (cart) {
+        check = cart.find((({ id }) => id === product.id));
+      } else {
+        cart = [];
+      }
+      if (check) {
+        Object.assign(product, { quantity: check.quantity + 1 });
+        cart = cart.filter(({ id }) => id !== check.id);
+        console.log(cart);
+      } else {
+        Object.assign(product, { quantity: 1 });
+      }
+      cart.push(product);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    } */
+
     render() {
-      const { product: { title, thumbnail, price } } = this.state;
+      const { product: { title, thumbnail, price/* , id */ } } = this.state;
       return (
         <div>
           <p data-testid="product-detail-name">
@@ -34,6 +54,18 @@ export default class ProductDetails extends Component {
           </p>
           <img src={ thumbnail } alt={ title } />
           <p>{ price }</p>
+          {/* <button
+            data-testid="shopping-cart-button"
+            type="button"
+            onClick={ () => this.addProductToCart({ title, price, thumbnail, id }) }
+          >
+            <Link
+              to="/Cart"
+              data-testid="product-detail-add-to-cart"
+            >
+              Adicionar ao Carrinho
+            </Link>
+          </button> */}
         </div>
       );
     }
