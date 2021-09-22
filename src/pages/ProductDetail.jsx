d11fc43bfe4c4f7babf049d5beb2d2f361b36fc4
PeDD1 <pedrohr0909@gmail.com>
2021-09-22 17:45:41 -0300

    Requirement done

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AddToCart from '../components/AddToCart';
+import CartButton from '../components/CartButton';
 
 class ProductDetail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItems: [],
+    };
+  }
+
+  componentDidMount() {
+    if (!JSON.parse(localStorage.getItem('setCart'))) {
+      localStorage.setItem('setCart', JSON.stringify([]));
+    }
+  }
+
+  componentDidUpdate() {
+    const { cartItems } = this.state;
+    localStorage.setItem('setCart', JSON.stringify(cartItems));
+  }
+
+  getItem = (item) => {
+    const { cartItems } = this.state;
+    const cart = cartItems;
+    this.setState({ cartItems: ([...cart, item]) });
+  }
+
   render() {
     const { match } = this.props;
     const { params } = match;
     const { title, price } = params;
 
     return (
-      <div data-testid="product">
-        <h2 data-testid="product-detail-name">{ title }</h2>
-        <p>{ price }</p>
-        <AddToCart
-          testId="product-detail-add-to-cart"
-          itemTitle={ title }
-          itemPrice={ price }
-        />
+      <div>
+        <CartButton />
+        <div data-testid="product">
+          <h2 data-testid="product-detail-name">{ title }</h2>
+          <p>{ price }</p>
+          <AddToCart
+            testId="product-detail-add-to-cart"
+            itemTitle={ title }
+            itemPrice={ price }
+            getItem={ this.getItem }
+          />
+        </div>
       </div>
     );
   }
@@ -26,6 +55,7 @@ ProductDetail.propTypes = {
   match: PropTypes.objectOf(PropTypes.object).isRequired,
   params: PropTypes.objectOf(PropTypes.object).isRequired,
   title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
 };
 
 export default ProductDetail;
