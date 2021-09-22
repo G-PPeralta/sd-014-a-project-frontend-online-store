cc149e7da5ca728a771d37817fdd98dff5f8e16f
Thiago Oshiro <oshirothiago@gmail.com>
2021-09-22 18:57:35 -0300

    teste

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { productsSave } from '../services/local';
+import { getCartProductsAndQuantity } from '../services/local'
+import { Link } from 'react-router-dom';
 
 class ProductDetail extends Component {
   constructor() {
     super();
-
     this.state = {
       product: { attributes: [] },
+      cartProducts: [],
+      itensQuantity: [],
     };
-
+    this.onClickBtn = this.onClickBtn.bind(this);
     this.getProductItemFromML = this.getProductItemFromML.bind(this);
+    this.getItens = this.getItens.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +24,33 @@ class ProductDetail extends Component {
         product: response,
       });
     });
+    this.getItens();
+  }
+
+  getItens() {
+    const cartInformation = getCartProductsAndQuantity();
+    console.log(cartInformation[0])
+    this.setState({ cartProducts: cartInformation[0], itensQuantity: cartInformation[1] });
+  }
+
+  onClickBtn({ target: { id } }) {
+    console.log('cliquei!')
+    console.log(id);
+    const { cartProducts, itensQuantity, product } = this.state;
+    console.log(cartProducts);
+    if (cartProducts === null) return console.log('erro!');
+    if (cartProducts.some((ele) => ele.id === id)) {
+      const quantity = itensQuantity;
+      quantity[id] += 1;
+      this.setState({ itensQuantity: quantity });
+      productsSave(cartProducts, quantity);
+    } else {
+      const cart = [...cartProducts, product];
+      const quantity = itensQuantity;
+      quantity[id] = 1;
+      this.setState({ cartProducts: cart, itensQuantity: quantity });
+      productsSave(cart, quantity);
+    }
   }
 
   async getProductItemFromML(id) {
@@ -29,10 +61,9 @@ class ProductDetail extends Component {
   render() {
     const { product } = this.state;
     const { attributes } = product;
-
     return (
-      <section data-testid="product-detail-name">
-        <h1>{ `${product.title}` }</h1>
+      <section>
+        <h1 data-testid="product-detail-name">{ `${product.title}` }</h1>
         <div>
           <img src={ product.thumbnail } alt={ product.title } />
         </div>
@@ -40,6 +71,16 @@ class ProductDetail extends Component {
           <h2>
             { `R$ ${product.price}` }
           </h2>
+          <button
+            id={ product.id } 
+            data-testid="product-detail-add-to-cart"
+            onClick={ this.onClickBtn }
+          >
+            Adicionar ao carrinho
+          </button>
+          <Link data-testid="shopping-cart-button" to="/shopping-cart">
+              Carrinho
+          </Link>
         </div>
         <div>
           <h2>Especificações Técnicas</h2>
