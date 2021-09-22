f515d7131e2c31f41641a2c3cb70c0f33220be39
leodiber <leonardodiber@gmail.com>
2021-09-21 21:13:02 -0300

    Adiciona o req8

--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import addToCart from '../services/localstorage';
 
 class ProductsCard extends Component {
   render() {
     const { product: { title, price, thumbnail } } = this.props;
+    const { product } = this.props;
     return (
       <li data-testid="product">
         <h1>{ title }</h1>
         <img src={ thumbnail } alt={ `${title}_image` } />
         <p>{ price }</p>
+        <button
+          type="button"
+          onClick={ () => addToCart(product) }
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao carrinho
+        </button>
       </li>
     );
   }
