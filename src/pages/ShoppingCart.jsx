9c6b9722d888834b9ac24d0f19b3a1b978a2fbdc
Matheus Pozett <maa_souza11@hotmail.com>
2021-09-22 17:48:31 -0300

    Requisito 8

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+    this.loadCart = this.loadCart.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadCart();
+  }
+
+  loadCart() {
+    this.setState({
+      products: JSON.parse(localStorage.getItem('products')),
+    });
+  }
+
   render() {
+    const { products } = this.state;
+    if (products.length === 0) {
+      return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
+    }
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {products.map((product) => (
+          <div
+            key={ product.id }
+          >
+            <h1 data-testid="shopping-cart-product-name">{ product.title }</h1>
+            <span data-testid="shopping-cart-product-quantity">{product.quantity}</span>
+          </div>
+        ))}
       </div>
     );
   }
