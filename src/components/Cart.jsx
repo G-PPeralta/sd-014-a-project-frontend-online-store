f492a9ba4f8dbbe6943ac4380df78fefcdea35ea
Amanda Zotelli <amzotelli@gmail.com>
2021-09-22 17:53:30 -0300

    requisito 10

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import CartProduct from './CartProduct';
 
 /* Pra formaatar o preço do produto, segui os passos de: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat */
-
-const formatPrice = (value) => new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-}).format(value);
 
 class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cart: [],
+      qtde: 1,
     };
     this.list = this.list.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.increase = this.increase.bind(this);
   }
 
   componentDidMount() {
@@ -27,6 +26,18 @@ class Cart extends Component {
     }
   }
 
+  decrease() {
+    this.setState((prevState) => ({
+      qtde: prevState.qtde - 1,
+    }));
+  }
+
+  increase() {
+    this.setState((prevState) => ({
+      qtde: prevState.qtde + 1,
+    }));
+  }
+
   render() {
     const { cart } = this.state;
     if (cart.length === 0) {
@@ -37,11 +48,7 @@ class Cart extends Component {
     return (
       <div>
         {cart.map((item) => (
-          <div key={ item.id }>
-            <p data-testid="shopping-cart-product-name">{item.id}</p>
-            <p>{formatPrice(item.price)}</p>
-            <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
-          </div>
+          <CartProduct key={ item.id } item={ item } />
         ))}
       </div>
     );
