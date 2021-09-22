4b77a9193626499622b6f709eaf47ad5ff498100
Paulo Eduardo <pauloeduardodesordigomes@gmail.com>
2021-09-22 11:24:59 -0300

    Add bootstrap and reactIcons

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GrCart } from 'react-icons/gr';
 
 class CartButton extends React.Component {
   render() {
     return (
-      <Link to="/cart" data-testid="shopping-cart-button">
-        Carrinho
+      <Link
+        to="/cart"
+        data-testid="shopping-cart-button"
+      >
+        <GrCart className="h1" />
       </Link>
     );
   }
