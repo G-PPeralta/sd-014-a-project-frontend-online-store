2ac286ec5b0e561b9cfd33b3fce0ffee993df716
Aline Carolina <85767848+AlineCarolina@users.noreply.github.com>
2021-09-22 21:49:21 -0300

    Revert "main"

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,7 @@
 export async function getCategories() {
-  const responseRaw = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const responseJson = await responseRaw.json();
-  return responseJson;
+  // Implemente aqui
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  let responseRaw = [];
-  if (categoryId && query) {
-    responseRaw = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-  } else if (categoryId) {
-    responseRaw = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
-  } else if (query) {
-    responseRaw = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-  }
-  const responseJson = await responseRaw.json();
-  return responseJson;
+export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
