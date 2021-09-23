222ecccf40b7e9a7f39e5e741c8abc98672872fc
JosueTavaress <josueferreira8.jf@gmail.com>
2021-09-23 19:05:02 -0300

    implement card list

--- src/Page/ProductDetails.jsx
+++ src/Page/ProductDetails.jsx
@@ -1,30 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FormReview from '../components/Reviews/FormReview';
+import CartButton from '../components/CartButton';
 
 export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: 0,
+      thumbnail: '',
+      title: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setaStateProduto();
+  }
+
+  setaStateProduto = () => {
+    const { match: { params: { id } } } = this.props;
+
+    const produtos = JSON.parse(localStorage.getItem('produtos'));
+
+    const findProduto = produtos.find((produto) => produto.id === id);
+
+    const { price, thumbnail, title } = findProduto;
+
+    this.setState({ price, thumbnail, title });
+  }
+
   render() {
-    const {
-      location: {
-        state: { price, title, thumbnail },
-      },
-    } = this.props;
+    const { price, thumbnail, title } = this.state;
+    const { addCart } = this.props;
     return (
       <div>
-        <h3 data-testid="product-detail-name">{`Produto${title} - R$ ${price}`}</h3>
-        <img src={ thumbnail } alt="thumbnail" />
+        <h3 data-testid="product-detail-name">
+          {`Produto${title} - R$ ${price}`}
+        </h3>
+        <img
+          src={ thumbnail }
+          alt="thumbnail"
+        />
         <FormReview />
+        <button
+          type="submit"
+          data-testid="product-detail-add-to-cart"
+          onClick={ (event) => addCart(event, title, price) }
+        >
+          adicionar ao carrinho
+        </button>
+        <CartButton />
       </div>
     );
   }
 }
 
 ProductDetails.propTypes = {
-  location: PropTypes.shape({
-    state: PropTypes.shape({
-      price: PropTypes.number,
-      title: PropTypes.string,
-      thumbnail: PropTypes.string,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
     }).isRequired,
   }).isRequired,
+  addCart: PropTypes.func.isRequired,
 };
