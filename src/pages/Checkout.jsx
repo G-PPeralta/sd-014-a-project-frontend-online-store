73e85cba956be53ce9292dc0315318307346d8ef
unamednada <dias.gustavo.dias@gmail.com>
2021-09-24 07:38:06 -0300

    Recupera carrinho do localstorage para pagina checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import '../services/localstorage';
 
 class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // cart: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getCart();
+  }
+
+  getCart() {
+    const cart = localStorage.getObj('products');
+    console.log(cart);
+  }
+
   render() {
     return (
       <h2>Checkout</h2>
