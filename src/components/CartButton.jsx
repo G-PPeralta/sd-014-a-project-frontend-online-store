36016d2c5b70aa30b0ef9d152efa938dae8c113a
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-10-02 18:43:35 -0300

    Ajuste no CartButton

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Cart } from 'react-bootstrap-icons';
 
-export default class extends Component {
+export default class CartButton extends Component {
   render() {
     return (
       <div id="cart-button-container">
