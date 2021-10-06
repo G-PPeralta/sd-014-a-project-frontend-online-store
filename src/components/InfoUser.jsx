dc07a7aebdd6638eaf194946e3df4a17d5c5a1e2
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-10-06 11:29:14 -0300

    Add datalist to states and city

--- src/components/InfoUser.jsx
+++ src/components/InfoUser.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import Estados from './Estados';
+// import Estados from './Estados';
+import cityAndStates from '../cityAndStates';
 
 class InfoUser extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      state: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value.toUpperCase() });
+  }
+
   render() {
+    const { state: curState } = this.state;
     return (
       <form>
         <input
@@ -17,8 +31,38 @@ class InfoUser extends React.Component {
         <input type="text" placeholder="Endereço" data-testid="checkout-address" />
         <input type="text" placeholder="Complemento" />
         <input type="text" placeholder="Número" />
-        <input type="text" placeholder="Cidade" />
-        <Estados />
+        <input
+          type="text"
+          list="states"
+          placeholder="Estado"
+          onChange={ this.handleChange }
+          name="state"
+          value={ curState }
+        />
+        <datalist id="states">
+          {cityAndStates.map((state, index) => (
+            <option
+              key={ state.sigla + index }
+              value={ state.sigla }
+            >
+              {state.nome.normalize('NFD').replace(/[\u0300-\u036f]/g, '')}
+            </option>
+          ))}
+        </datalist>
+        <input type="text" list="city" />
+        <datalist id="city">
+          {cityAndStates.filter((state) => curState === '' || state.sigla === curState)
+            .map((state) => state.cidades.map((city, index) => (
+              <option
+                key={ city + index }
+                value={ city }
+              >
+                {city.normalize('NFD').replace(/[\u0300-\u036f]/g, '')}
+              </option>
+            )))}
+        </datalist>
+        {/* <input type="text" placeholder="Cidade" /> */}
+        {/* <Estados /> */}
       </form>
     );
   }
